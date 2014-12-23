e45571ac4bca95cfc143b6a88cd14a2c29fda080
apetrovskiy <apetrovskiy@hotmail.com>
2014-12-23 22:42:39 +0300

    .

--- tinyIocTest/Modules/TestModule.cs
+++ tinyIocTest/Modules/TestModule.cs
@@ -26,9 +26,25 @@ namespace tinyIocTest.Modules
             Get["/"] = _ => {
 Console.WriteLine("001");
                 dynamic expando = new ExpandoObject();
-                expando.Test01 = TinyIoCContainer.Current.Resolve<I01>();
+Console.WriteLine("002");
+                expando.Test = "test";
+Console.WriteLine("003");
+                try {
+Console.WriteLine("004");
+                    expando.Test01 = TinyIoCContainer.Current.Resolve<I01>();
+                    expando.Test01.Name = "via interface";
+Console.WriteLine("005");
+                }
+                catch (Exception ee) {
+                    Console.WriteLine(ee.Message);
+                }
+Console.WriteLine("006");
                 expando.Test02 = TinyIoCContainer.Current.Resolve<Class02>();
+                expando.Test02.Name = "via class (the only option)";
+Console.WriteLine("007");
                 expando.Test03 = TinyIoCContainer.Current.Resolve<Class01>();
+                expando.Test03.Name = "via class";
+Console.WriteLine("008");
                 return View["Test", expando];
             };
         }
