e8a85eb06dccb694783c7d063bdb967a7beb84f3
Alexander Petrovskiy <apetrovskiy@hotmail.ocm>
2015-03-03 02:15:13 -0800

    the DataExchange example

---
+++ DataExchange/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alexander Petrovskiy
+ * Date: 3/3/2015
+ * Time: 12:02 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+namespace DataExchangeService
+{
+	using System;
+	using Nancy;
+	using Nancy.Hosting.Self;
+	
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+            var host = new NancyHost (new Uri ("http://localhost:12340"));
+            StaticConfiguration.DisableErrorTraces = false;
+            host.Start ();
+
+            Console.WriteLine ("Press any key to stop server...");
+            Console.ReadKey ();
+		}
+	}
+}
