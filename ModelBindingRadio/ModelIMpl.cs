63909422cb0c401b9e072319ab3737ff69ce1d2e
apetrovskiy <apetrovskiy@hotmail.com>
2015-07-13 13:03:25 +0300

    .

---
+++ ModelBindingRadio/ModelIMpl.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alexander Petrovskiy
+ * Date: 01/07/2015
+ * Time: 10:21 p.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+namespace ModelBindingRadio
+{
+    using System;
+    
+    /// <summary>
+    /// Description of ModelImpl.
+    /// </summary>
+    public class ModelImpl
+    {
+        public string Inputs001 { get; set; }
+    }
+}
